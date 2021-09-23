5247e30a5e1efe5107c6d40fd41097bc0a034003
Diwaker323 <diwakersingh415@gmail.com>
2021-09-23 09:35:22 +0530

    made a new comment

---
+++ src/com/diwaker/sample.java
@@ -0,0 +1,16 @@
+package com.diwaker;
+
+public class sample {
+    public static void main (String[] args){
+        int i , j;
+        for(i = 0 ; i < 10; i++)
+        {
+
+            for( j = 0 ; j <= i; j++)
+            {
+                System.out.println(+i);
+            }
+            System.out.println("\n");
+        }
+    }
+}
